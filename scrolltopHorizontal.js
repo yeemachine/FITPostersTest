b0b7323aa8ee31c3b88c1bc61728546a2c6a72cd
yeemachine <richtomyee@gmail.com>
2017-06-08 11:54:52 -0400

    fixed scrolling bug

--- scrolltopHorizontal.js
+++ scrolltopHorizontal.js
@@ -1,46 +1,38 @@
-$(window).scroll(function() {
+$( document ).ready(function() {
 
-  var windowPosX = $(this).scrollLeft();
-  var windowPosY = $(this).scrollTop();
-  var page1X = $('.page1').width()/2;
-  var page1Y = $('.page1').height()/2;
-  var lastScrollLeft = windowPosX;
-  var lastScrollTop = windowPosY;
 
   var isMobile = window.matchMedia("only screen and (max-width: 800px)");
-  if (isMobile.matches) {
-    window.addEventListener("scroll", function() {
-      var st = window.pageYOffset || document.documentElement.scrollTop;
-      if (st > lastScrollTop && $(this).scrollTop()>page1Y && $('body').attr('status') != 'locked') {
-        // downscroll code
-        // console.log('scrolldown')
-        $('.main-nav').css({'margin-top':'-10vh'})
-      } else {
-        // console.log('scrollup')
-        $('.main-nav').css({'margin-top':''})
-        // upscroll code
-      }
-      lastScrollTop = st
-    }, false);
-  }else{
-    window.addEventListener("scroll", function() {
-      var st = window.pageXOffset || document.documentElement.scrollLeft;
-      if (st > lastScrollLeft && $(this).scrollLeft()>page1X && $('body').attr('status') != 'locked') {
-        // downscroll code
-        // console.log('scrolldown')
-        $('.main-nav').css({'margin-top':'-10vh'})
-      } else {
-        // console.log('scrollup')
-        $('.main-nav').css({'margin-top':''})
-        // upscroll code
-      }
-      lastScrollLeft = st
-    }, false);
-  }
-
-
-
-
+  var page1X = $('.page1').width()/2;
+  var page1Y = $('.page1').height()/2;
+    if (isMobile.matches) {
+      var lastScrollTop = 0;
+      $('#lazy-container').on('scroll', function() {
+          st = $(this).scrollTop();
+          if(st > lastScrollTop && $(this).scrollTop()>page1Y && $('body').attr('status') != 'locked' ) {
+            // console.log('down 1');
+            $('.main-nav').css({'margin-top':'-10vh'})
+          }
+          else {
+            // console.log('up 1');
+            $('.main-nav').css({'margin-top':''})
+          }
+          lastScrollTop = st;
+      });
+    }else{
+        var lastScrollLeft = 0;
+        $('#lazy-container').on('scroll', function() {
+            st = $(this).scrollLeft();
+            if(st > lastScrollLeft && $(this).scrollLeft()>page1X && $('body').attr('status') != 'locked' ) {
+              // console.log('down 1');
+              $('.main-nav').css({'margin-top':'-10vh'})
+            }
+            else {
+              // console.log('up 1');
+              $('.main-nav').css({'margin-top':''})
+            }
+            lastScrollLeft = st;
+        });
+    }
 
 
 });
