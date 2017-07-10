7e19125786ce1c3fccce59aac05b587b75ecb86b
yeemachine <richtomyee@gmail.com>
2017-07-10 15:23:28 -0400

    d

--- lazy.js
+++ lazy.js
@@ -1,6 +1,6 @@
 $(function() {
        $('#lazy-container .lazy').lazy({
-           appendScroll: $('#lazy-container'),
+           appendScroll: $('#lazy-container, body'),
           //  effect: "fadeIn",
           //  effectTime: 500,
           //  threshold: 0
