f425a210e9380018418c6bf6cbcfc5faceb990ae
yeemachine <richtomyee@gmail.com>
2017-07-10 11:00:11 -0400

    d

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ $( document ).ready(function() {
         // addCart();
       }
    });
-   $('.quantity').click(function() {
+   $('.imgsquare.cartMode').click(function() {
      addCart();
     });
     // $('.minus').click(function() {
