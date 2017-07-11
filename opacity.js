32daebb4b3b224cfff28661dc3827e4c3995ce66
yeemachine <richtomyee@gmail.com>
2017-07-11 16:53:58 -0400

    d

--- opacity.js
+++ opacity.js
@@ -3,21 +3,31 @@ console.log('hello there');
 var header = $('.home');
 var home = $('.home');
 var range = 50;
+var range2 = 100;
 
 $('#lazy-container').on('scroll', function () {
-  console.log('this works');
-  var scrollTop = $(this).scrollTop(),
-      height = header.height(),
-      offset = height,
-      calc = 1 - (scrollTop - offset + range) / range;
+  if (state === 'mobile'){
+  }else{
+    console.log('this works');
+    var scrollTop = $(this).scrollTop(),
+        height = header.height(),
+        offset = height,
+        calc = 1 - (scrollTop - offset + range) / range;
+        calc2 = 1 - (scrollTop - offset*2 + range2) / range2;
 
-  home.css({ 'opacity': calc });
+    home.css({ 'opacity': calc });
+    $('.bookIntro h3').css({ 'opacity': calc2 });
 
-  if (calc > '1') {
-    home.css({ 'opacity': 1 });
-  } else if ( calc < '0' ) {
-    home.css({ 'opacity': 0 });
+    if (calc > '1') {
+      home.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+      home.css({ 'opacity': 0 });
+    }
+    if (calc2 > '1') {
+        $('.bookIntro h3').css({ 'opacity': 1 });
+    } else if ( calc2 < '0' ) {
+        $('.bookIntro h3').css({ 'opacity': 0 });
+    }
   }
-
 });
 });
